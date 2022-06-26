424b294f68bdfd9bd495964a52dd839b4a4e3330
LIME <95404736+ilimes@users.noreply.github.com>
2022-06-26 21:45:50 +0900

    commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "github.io 블로그 첫 포스팅입니다",
+        "excerpt":"마크다운 문법으로 글을 작성할 수 있습니다.  마크다운 설명 글 보기  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/post/",
+        "teaser": null
+      },{
+        "title": "마크다운(Markdown) 문법",
+        "excerpt":"마크다운(Markdown) 이란? 텍스트 기반의 마크업 언어로 GitHub에서 README 파일을 편집할 때 많이 쓰인다. 가독성이 좋고 간단해서 배우기 쉽다. 제목 # 제목 1단계 ## 제목 2단계 ### 제목 3단계 #### 제목 4단계 ##### 제목 5단계 ###### 제목 6단계 제목 1단계 제목 2단계 제목 3단계 제목 4단계 제목 5단계 제목 6단계 강조...","categories": [],
+        "tags": ["Blog","Markdown","Jekyll"],
+        "url": "/post2/",
+        "teaser": null
+      }]
